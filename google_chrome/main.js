e7add6305cec73b1e2401f7fda754e7030aae925
Julio Cesar Vera Neto <neto-vera@hotmail.com>
2021-07-08 11:46:32 -0300

    Backup

--- google_chrome/main.js
+++ google_chrome/main.js
@@ -1,6 +1,16 @@
-let color = '#3aa757';
+const langSystem = navigator.language;
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+const language = () => {
+
+};
+
+chrome.storage.sync.set({ langSystem: langSystem.replace("-", "_") }, () => {});
+
+
+
+
+
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.storage.sync.set({ color });
+//     console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
